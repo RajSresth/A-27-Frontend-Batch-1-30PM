8525a9b3e457726e0f906e51843af0c91b432523
Shresth Rajput <rajshresth2@gmail.com>
2025-09-15 15:32:30 +0530

    Promise Part-2

--- JavaScript/Day-30 Promise/script.js
+++ JavaScript/Day-30 Promise/script.js
@@ -1,6 +1,6 @@
 /**
  * ! Promise() constructor
- */
+
 
 const p1= new Promise(function task(resolve,reject)
                       {
@@ -10,7 +10,53 @@ const p1= new Promise(function task(resolve,reject)
                       });
 
 console.log("P1:",p1);
+ */
+
+/**
+ * ! Example-2
+ */
+
+const p1= new Promise((resolve,reject) =>{
+                   setTimeout(() => {
+                                    reject("Promise pura nhi hua..");
+                                    // resolve("Chombu")
+                              }, 5000);           
+            });
 
 
-console.log({}+"")  
-                    
+p1
+.then(
+      function success1(val)
+      {
+            val= val + "Singh";
+            return val;
+      },
+      function error1(reason)
+      {
+            console.log(reason);
+            // throw new Error("Something went wrong..");
+          
+            const p2= new Promise((resolve,reject) =>{
+                 setTimeout(() => {
+                         reject("Promise firse pura nahi hua..")
+                 }, 5000);
+            })
+            return p2
+      }
+)           
+.then(
+      function success2(fullname)
+      {
+            console.log(fullname)
+      },
+      function error2(reason)
+      {
+            console.log(reason)
+      }
+)
+.catch(function globalError(err){
+      console.log(err.message)
+})
+
+
+
