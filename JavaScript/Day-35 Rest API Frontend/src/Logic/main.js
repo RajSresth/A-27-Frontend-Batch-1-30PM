0a63bfb4bb12111ba761d85e171c43ce8929385d
Shresth Rajput <rajshresth2@gmail.com>
2025-10-09 14:27:44 +0530

    Todo App Completed

--- JavaScript/Day-35 Rest API Frontend/src/Logic/main.js
+++ JavaScript/Day-35 Rest API Frontend/src/Logic/main.js
@@ -1,17 +1,77 @@
-import { todo_blueprint, todos_URL } from "../utils/constants.js"
-import { getTodos } from "./app.js";
+import { todos_URL } from "../utils/constants.js"
+import { createTodo, deleteTodo, getTodos } from "./app.js";
 
+// Dom Selectors
 
-const todos = await getTodos(todos_URL);
-console.log("todos:",todos);
- 
-const y = todos.map(element => {
-   const {task} = element;
-   return todo_blueprint(task);   // li -- take batch
+const input = document.getElementById("input");
+const addButton = document.getElementById("addBtn");
+
+// create Todo
+addButton.addEventListener("click",async () => {
+   addButton.disabled = true;
+   const task = input.value.trim();
+
+   if (!task) {
+      alert("Please enter a valid task!");
+      addButton.disabled = false;
+      return;
+   }
+   const newTodo ={
+      task,
+      complete:false
+   }
+
+   try{
+         await createTodo(todos_URL , newTodo);   // POST Network Request ---> 201
+         await getTodos(todos_URL);               //  GET Network Request  ---> 200
+   }   
+   catch(err)
+   {
+         console.error("Error creating todo:", err);
+         alert("Something went wrong! Try again.");
+   }
+   finally{
+      addButton.disabled= false;
+      input.value="";
+   }
+
 });
 
-const todolist = document.querySelector(".todolist")
-todolist.innerHTML +=y.join(" ");  
 
+input.addEventListener("keydown",async (e) => {
+   if(e.key === "Enter")
+   {
+      addButton.disabled = true;
+      const task = input.value.trim();
 
+         if (!task) {
+            alert("Please enter a valid task!");
+            addButton.disabled = false;
+            return;
+         }
+         const newTodo ={
+            task,
+            complete:false
+         }
+
+         try{
+               await createTodo(todos_URL , newTodo);   // POST Network Request ---> 201
+               await getTodos(todos_URL);               //  GET Network Request  ---> 200
+         }   
+         catch(err)
+         {
+               console.error("Error creating todo:", err);
+               alert("Something went wrong! Try again.");
+         }
+         finally{
+            addButton.disabled= false;
+            input.value="";
+         }
+   }
+
+})
+
+getTodos(todos_URL);
+
+ 
 
